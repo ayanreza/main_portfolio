156302330acec3a431ced9dee561cf39cdbf492e
Ayan <ayanreza0@gmail.com>
2021-10-24 01:03:56 -0400

    added avatar image to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,20 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Footer from '../components/Footer'
+import ContainerBlock from '../components/ContainerBlock'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import Footer from '../components/Footer'
 
 const Home: NextPage = () => {
   return (
     <div>
-
-      <Navbar />
-      <Footer />
+      <ContainerBlock 
+      title="Ayan Reza - Developer, Programmer, Writer, Creator"
+      description="This is my portfolio website showing my projects"
+      >
+        <Hero />
+      </ContainerBlock>
     </div>
   )
 }
