591eb5bc0a01cb3d9067d4e9dc40d20dda799fbe
Ayan <ayanreza0@gmail.com>
2021-11-13 00:19:10 -0500

    configured files to utilize github API and display latest code on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,23 +4,36 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import ContainerBlock from '../components/ContainerBlock'
 import Navbar from '../components/Navbar'
+import LatestCode from '../components/LatestCode'
+import userData from '../constants/data'
+import getLatestRepos from '../lib/getLatestRepos'
 import Hero from '../components/Hero'
 import FavouriteProjects from '../components/FavoriteProjects'
 import Footer from '../components/Footer'
 
-const Home: NextPage = () => {
+export default function Home({ repositories }) {
   return (
-    <div>
-      <ContainerBlock 
-      title="Ayan Reza - Developer, Programmer, Writer, Creator"
-      description="This is my portfolio website showing my projects"
-      >
-        <Hero />
-        <FavouriteProjects/>
-
-      </ContainerBlock>
-    </div>
-  )
+    <ContainerBlock
+      title="Manu Arora - Developer, Writer, Creator"
+      description="This is a template built specifically for my blog - Creating a developer portfolio that gets you a job."
+    >
+      <Hero />
+      <FavouriteProjects />
+      <LatestCode repositories={repositories} />
+    </ContainerBlock>
+  );
 }
 
-export default Home
+export const getServerSideProps = async () => {
+  console.log(process.env.GITHUB_AUTH_TOKEN);
+  let token = process.env.GITHUB_AUTH_TOKEN;
+
+  const repositories = await getLatestRepos(userData, token);
+  // console.log("REPOSITORIES", repositories);
+
+  return {
+    props: {
+      repositories,
+    },
+  };
+};
