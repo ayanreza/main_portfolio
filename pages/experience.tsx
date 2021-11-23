ad48b5dd43ec2ed1bae2e25cf8966007f50df8a1
Ayan <ayanreza0@gmail.com>
2021-11-23 01:50:49 -0500

    deleted a wrongly pasted line of code

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -1,5 +1,3 @@
- title="Ayan Reza - Developer, Programmer, Innovator, Student"
-      description="Ayan Reza's personal portfolio site displaying his projects and skills"import React from "react";
 import ContainerBlock from "../components/ContainerBlock";
 import Experience from "../components/Experience";
 
