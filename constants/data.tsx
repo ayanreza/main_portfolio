fab4d1930d59a265e18482340ab6e248be0c3810
Ayan <ayanreza0@gmail.com>
2021-11-23 00:35:42 -0500

    updated userData by adding up to date work experience and projects to show on website

--- constants/data.tsx
+++ constants/data.tsx
@@ -1,115 +1,66 @@
 const userData = {
   githubUsername: "ayanreza",
   name: "Ayan Reza",
-  designation: "Full-Stack Developer",
-  avatarUrl: "/avatar.png",
-  email: "manuarorawork@gmail.com",
-  phone: "+91 9587738861",
-  address: "Jaipur, Rajasthan, India.",
+  email: "ayanreza@gmail.com",
+  phone: "516-469-5883",
   projects: [
     {
-      title: "Tailwind Master Kit",
-      link: "https://tailwindmasterkit.com",
+      title: "Main Portfolio",
+      link: "https://github.com/ayanreza/main_portfolio",
       imgUrl: "/tmk.jpg",
     },
     {
-      title: "PlaceholderTech",
-      link: "https://placeholdertech.in",
+      title: "Guardian",
+      link: "https://devpost.com/software/guardian-rw4lvg",
       imgUrl: "/placeholdertech.png",
     },
     {
-      title: "Portfolio",
-      link: "https://manuarora.in",
+      title: "Parstagram",
+      link: "https://github.com/ayanreza/parstagram",
       imgUrl: "/portfolio.png",
-    },
-    {
-      title: "CovidRescue",
-      link: "https://covidrescue.co.in",
-      imgUrl: "/covidrescue.png",
-    },
-    {
-      title: "Algochurn",
-      link: "https://algochurn.com",
-      imgUrl: "/algochurn.png",
-    },
-    {
-      title: "Playground",
-      link: "https://play.placeholdertech.in",
-      imgUrl: "/playground.png",
-    },
-    {
-      title: "Feedmeback",
-      link: "https://feedmeback.vercel.app",
-      imgUrl: "/feedmeback.png",
-    },
-    {
-      title: "Devmedium",
-      link: "https://devmedium.vercel.app",
-      imgUrl: "/devmedium.png",
     },
   ],
   about: {
     title:
-      "I'm a software developer that loves building products and web applications that impact millions of lives",
+      "I am a Software Engineer who loves collaborating with teams to make products which have a positive impact on the world.",
     description: [
-      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
-      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
-      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+      `I have been coding since highschool and am scared yet fascinated as to how the world is changing so rapidly. I have been experimenting with many technologies from Mobile, Web, Cloud, and Data in an effort to find what specialization I should pursue during the rest of my Software Engineering Career.`,
     ],
-    currentProject: "Tailwind Master Kit",
-    currentProjectUrl: "https://tailwindmasterkit.com",
+    currentProject: "Applying for Internships!!",
+    currentProjectUrl: "https://drive.google.com/file/d/1D42ZqlNHmJuNCZvKgYV0PMjLEHGApUbv/view?usp=sharing",
   },
   experience: [
     {
-      title: "Software Developer",
-      company: "mroads",
+      title: "iOS Tech Fellow",
+      company: "CodePath",
       year: "2021",
-      companyLink: "https://mroads.com",
-      desc: "Contributed to our flagship product - Panna - which is an AI powered candidate interviewing product.",
+      companyLink: "https://codepath.org",
+      desc: `Managed, led, and taught 30+ students in complex iOS Development topics such as MVC Frameworking, RESTful
+      APIs, WebKit, MapKit, and Parse Databasing by designing comprehensive lesson plans and interactive activities to
+      promote learning in class
+      • Selected to join 80+ iOS Development Tech Fellows out of 300+ applicants in a semester long iOS Development
+      Program, endorsed by companies like Google, Amazon, Facebook, and Microsoft, based on self-taught technical
+      skills and leadership potential
+      • Guided students to develop a custom iOS application in teams of 3 by using agile/scrum development frameworks, numerous iOS Libraries (AlamoFire/AlamoFireImage, SwiftyJSON), and APIs (Twitter API, Parse API, Movies API) within a 2 week development period`,
     },
     {
-      title: "Founder and Developer",
-      company: "TailwindMasterKit",
+      title: "Software Engineering Fellow",
+      company: "TheHeadStarter",
       year: "2021",
-      companyLink: "https://tailwindmasterkit.com",
-      desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
-    },
-    {
-      title: "Founder and Developer",
-      company: "Algochurn",
-      year: "2021",
-      companyLink: "https://algochurn.com",
-      desc: "Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.",
-    },
-    {
-      title: "Internship",
-      company: "Intel Corporation",
-      year: "2019",
-      companyLink: "https://intel.com",
-      desc: "Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.",
-    },
-    {
-      title: "Graduation",
-      company: "UPES, Dehradun.",
-      year: "2019",
-      companyLink: "https://upes.co.in",
-      desc: "Major in Computer Science and Engineering with a CGPA of 7.0. Nobody asks this but it's okay.",
-    },
-    {
-      title: "High School",
-      company: "Delhi Public School, Jaipur.",
-      year: "2015",
-      companyLink: "https://upes.co.in",
-      desc: "Subjects being PCM - I barely survived with 93.6% marks aggregate. Flex fridays, fellas.",
+      companyLink: "https://www.theheadstarter.com",
+      desc: `Built serverless APIs using AWS Lambda with 99% uptime and real-time DynamoDB data entry tables
+      ● Developed projects from design to deployment leading 4+ fellows using MVC design patterns
+      ● Coached by Amazon, Bloomberg, and Capital One engineers on Agile, CI/CD, Git, and microservice
+      patterns`,
     },
   ],
   resumeUrl:
-    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+    "https://drive.google.com/file/d/1D42ZqlNHmJuNCZvKgYV0PMjLEHGApUbv/view?usp=sharing",
   socialLinks: {
-    instagram: "https://instagram.com/maninthere",
-    twitter: "https://twitter.com/mannupaaji",
-    linkedin: "https://linkedin.com/in/manuarora28",
-    github: "https://github.com/manuarora700",
+    instagram: "https://www.instagram.com/ayanpoptosis/",
+    twitter: "https://twitter.com/theAyanReza",
+    linkedin: "https://linkedin.com/in/ayanreza",
+    github: "https://github.com/ayanreza",
     facebook: "https://facebook.com/",
   },
 };
