e005828c68221583f48625c0d3f39ecef718f595
Ayan <ayanreza0@gmail.com>
2021-11-23 01:00:43 -0500

    added projects page with present projects inputed in userData

---
+++ pages/projects.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Projects from "../components/Projects";
+import ContainerBlock from "../components/ContainerBlock";
+
+export default function projects() {
+    return (
+        <ContainerBlock>
+            <Projects/>
+        </ContainerBlock>
+    )
+}
